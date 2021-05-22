2f1a59ecde063eba045d4d0e093d7fc303b097d0
Eric-JG <guerrero.eric.j@gmail.com>
2021-05-22 12:04:25 -0400

    added the questions

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+const quizData = [
+    {
+        question: "What does CSS stand for?",
+        1:"Cascading Style Sheets",
+        2:"Cascading Simple Sheets",
+        3:"Central Style Sheets",
+        4:"Cars SUVs Sailboats",
+        correct:"1",
+    },
+    {
+        question: "What does HTML stand for?",
+        1:"Hypertext Markdown Language",
+        2:"Hyperloop Machine Language",
+        3:"Hypertext Markup Language",
+        4:"Helicopter Terminals Motorcicle",
+        correct:"3",
+    },
+    {
+        question:"In what year JavaScript launched",
+        1:"1999",
+        2:"2000",
+        3:"1994",
+        4:"1995",
+        correct:"4",
+    },
+    {
+        question:"Who created Bootstrap",
+        1:"Twitter",
+        2:"Amazon",
+        3:"Facebook",
+        4:"None of the above",
+        correct:"1",
+    },
+    {
+        question:"When HTML was invented",
+        1:"1990",
+        2:"1993",
+        3:"2000",
+        4:"2021",
+        correct:"2",
+    },
+];
+
+var quiz = document.getElementById('quiz')
